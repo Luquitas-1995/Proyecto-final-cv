052b7e38f18185f83c3c2e1ea64257483fd3ec42
Luquitas-1995 <lucasmariano@outlook.com>
2023-02-08 14:39:32 -0300

    Se agrega el Proyecto

---
+++ function/function.js
@@ -0,0 +1,12 @@
+
+console.log("Hola mundo");    
+
+function mostrar (){
+	document.getElementById('links_2_id').style.display = 'block';
+	console.log("capturamos el evento ver redes");
+};
+
+function ocultar (){
+	document.getElementById('links_2_id').style.display = 'none';
+	console.log("capturamos el evento ocultar redes");
+};
